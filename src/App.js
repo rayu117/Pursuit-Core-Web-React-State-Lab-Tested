f14bd6fb1d22641ed36d32e1a3763286db9a6a9a
Raymond Udeogu <rfizzle25@gmail.com>
2021-04-23 14:41:55 -0400

    state-lab

--- src/App.js
+++ src/App.js
@@ -3,8 +3,31 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      totalNumber : 0
+    }
+    
+  }
+
+  handleClick = () => {
+    this.totalNumber +=1
+
+    this.setState({
+      totalNumber: this.state.totalNumber + 1
+    })
+
+  }
   render() {
-    return "Hello, world!";
+    return (
+      <div>
+        <h1>Current Score: {this.state.totalNumber}</h1>
+        <button onClick={this.handleClick}>+1</button>
+        <button>Pay 10 points to increase from +1 to +2</button>
+      </div>
+    )
   }
 }
 
